e53e13a8a12eb1534e9046314e97e7234704b718
Étienne Michon <etienne@scalingo.com>
2016-12-30 19:44:46 +0100

    more logs

--- influx/influx.go
+++ influx/influx.go
@@ -1,6 +1,8 @@
 package influx
 
 import (
+	"log"
+
 	"github.com/Scalingo/sample-influxdb/config"
 	influx "github.com/influxdata/influxdb/client/v2"
 
@@ -8,6 +10,7 @@ import (
 )
 
 func Client() (influx.Client, error) {
+	log.Printf("Client information: %+v\n", config.InfluxConnectionInformation)
 	client, err := influx.NewHTTPClient(influx.HTTPConfig{
 		Addr:     config.InfluxConnectionInformation.Host,
 		Username: config.InfluxConnectionInformation.User,
